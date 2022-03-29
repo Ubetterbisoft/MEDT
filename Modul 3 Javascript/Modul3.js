9be0378940a0e134c0c02dd6737f82792ceba594
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2022-03-29 13:19:14 +0200

    Update Modul3.js

--- Modul 3 Javascript/Modul3.js
+++ Modul 3 Javascript/Modul3.js
@@ -1,22 +1,27 @@
-let elem = document.getElementById('content');
-    let lipsum = document.getElementById('lipsum');
-    /** console.log(elem.parentNode)
-    console.log(elem.childNodes)
-    console.log(elem.previousSibling)
-    console.log(elem.nextSibling)
-    console.log(lipsum.parentNode.parentNode.childNodes[7]);
-    */
-    
-    let footer = document.getElementById('footer');
-    function doSomething() {
-        footer.innerHTML = event.clientX + " / " + event.clientY;
-    }
-    function red() {
-        footer.style.backgroundColor = '#FF0000';
-    }
-    function white() {
-        footer.style.backgroundColor = '#FFFFFF';
-    }
-    lipsum.addEventListener('mousemove',doSomething);
-    lipsum.addEventListener('mouseover', red);
-    lipsum.addEventListener('mouseout', white)
+function red() {
+               return "#ff0000"
+            }
+            function yellow(){
+               return "#ffff00"
+            }
+
+
+            
+            let footer = document.getElementById('footer');
+            function changeColor(){
+                footer.style.backgroundColor = red();
+            }
+            function changeColor2(){
+               footer.style.backgroundColor = yellow();
+            }
+         
+           
+            
+            function doSomething() {
+                footer.innerHTML = event.clientX + " / " + event.clientY;
+            }
+
+            let lipsum = document.getElementById('lipsum');
+            lipsum.addEventListener('mousemove',doSomething);
+            lipsum.addEventListener('mouseover', changeColor);
+            lipsum.addEventListener('mouseout',changeColor2);
