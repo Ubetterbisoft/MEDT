b367dcf68afaeb75447012ddde99d5f122a957c5
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2022-05-24 20:52:50 +0200

    Formulare Js

--- Formulare & DOM/Js/index.js
+++ Formulare & DOM/Js/index.js
@@ -7,36 +7,46 @@ let farbe = "";
 
 function spiceBar(wert){
 
-
+	//der balken wird nicht zu groß wenn man einen großen wert eingibt
 	wert = Number(wert);
-	let reale_breite = (breite/100)*wert;
+	let reale_breite = 0;
+	if(wert >= 4){
+		reale_breite = (breite/5)*4;
+	}else if(wert < 4 && wert> 0){
+		reale_breite = (breite/5)*wert;
+	}
+	
 
     document.getElementById("Beschreibung").style.width = reale_breite + "px";
 
     document.getElementById("nf").value = wert;
 
     document.getElementById("x").value = wert;
-
-    if (wert>70)
-			{ 	//grün
-				document.getElementById("Beschreibung").style.backgroundColor="#00ff00";
+		
+    		if (wert>0&&wert <= 1)
+			{ 	//blau
+				document.getElementById("Beschreibung").style.backgroundColor="#0000FF";
 				//setzen der globalen Variable für weitere Funktionen
-				farbe = "#00ff00";
+				farbe = "#0000FF";
+
+				
+				
 			}
-			if (wert>50&&wert<71)
+			if (wert>1&&wert<=2)
+			{	//grün
+				document.getElementById("Beschreibung").style.backgroundColor="#00ff00";
+				farbe = "##00ff00";
+				
+			}	
+			if (wert>=3&&wert<4)
 			{	//gelb
 				document.getElementById("Beschreibung").style.backgroundColor="#ffff00";
 				farbe = "#ffff00";
-			}	
-			if (wert>30&&wert<51)
-			{	//orange
-				document.getElementById("Beschreibung").style.backgroundColor="#ff8000";
-				farbe = "#ff8000";
-			}	
-			if (wert<31)
-			{	//rot
+			}
+			if(wert == 4){
 				document.getElementById("Beschreibung").style.backgroundColor="#ff0000";
 				farbe = "#ff0000";
 			}	
+			
 
 }
